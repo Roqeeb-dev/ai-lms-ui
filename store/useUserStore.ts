1c812c3581299479461f9ecd51c1a6e234ce424d
Roqeeb <shafiriyuroqeeb@gmail.com>
2026-02-28 00:42:26 +0100

    initialized user store in zustand

--- store/useUserStore.ts
+++ store/useUserStore.ts
@@ -1,1 +1,17 @@
+import { User } from "@/types/user";
 import { create } from "zustand";
+
+interface UserStore {
+  user: User | null;
+  updateUserDetails: (data: Partial<User>) => void;
+}
+
+export const useUserStore = create<UserStore>((set) => ({
+  user: null,
+
+  updateUserDetails: (data) => {
+    set((state) => ({
+      user: state.user ? { ...state.user, ...data } : null,
+    }));
+  },
+}));
