056db68d6a63319b7cde30c3d0bf134f2cbe7f4d
Roqeeb <shafiriyuroqeeb@gmail.com>
2026-03-02 13:56:52 +0100

    added the api client

--- lib/apiClient.ts
+++ lib/apiClient.ts
@@ -1,6 +1,14 @@
-export class apiClient {
-  async request<T>(url: string, options?: RequestInit): Promise<T> {
-    const res = await fetch(url, options);
+const baseUrl = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:5173";
+
+export class ApiClient {
+  async request<T>(url: string, options: RequestInit = {}): Promise<T> {
+    const res = await fetch(baseUrl + url, {
+      headers: {
+        "Content-Type": "application/json",
+        ...options.headers,
+      },
+      ...options,
+    });
 
     if (!res.ok) {
       throw new Error("Request failed");
@@ -11,5 +19,25 @@ export class apiClient {
 
   get<T>(url: string) {
     return this.request<T>(url);
+  }
+
+  post<T, B>(url: string, body: B) {
+    return this.request<T>(url, {
+      method: "POST",
+      body: JSON.stringify(body),
+    });
+  }
+
+  put<T, B>(url: string, body: B) {
+    return this.request<T>(url, {
+      method: "PUT",
+      body: JSON.stringify(body),
+    });
+  }
+
+  delete<T>(url: string) {
+    return this.request<T>(url, {
+      method: "DELETE",
+    });
   }
 }
