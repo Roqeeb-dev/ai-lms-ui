b184ae0db11183aa54ff21cf9fa7190bb839f42b
Roqeeb <shafiriyuroqeeb@gmail.com>
2026-03-02 14:12:23 +0100

    added the auth service too

---
+++ lib/authService.ts
@@ -0,0 +1,19 @@
+import { apiClient } from "./apiClient";
+import { User } from "@/types/user";
+
+type LoginPayload = Pick<User, "email" | "password">;
+type RegisterPayload = Pick<User, "fullname" | "email" | "password" | "role">;
+type AuthResponse = {
+  token: string;
+  user: User;
+};
+
+export const auth = {
+  async login(payload: LoginPayload) {
+    return apiClient.post<AuthResponse, LoginPayload>("/login", payload);
+  },
+
+  async register(payload: RegisterPayload) {
+    return apiClient.post<AuthResponse, RegisterPayload>("/register", payload);
+  },
+};
