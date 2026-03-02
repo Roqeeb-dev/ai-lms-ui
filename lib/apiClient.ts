35ef06c500f24d0f5f2651a65426a898cc7c9743
Roqeeb <shafiriyuroqeeb@gmail.com>
2026-03-02 14:03:24 +0100

    rewrote the apiclient to be function based and nt class based

--- lib/apiClient.ts
+++ lib/apiClient.ts
@@ -1,6 +1,6 @@
 const baseUrl = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:5173";
 
-export class ApiClient {
+export const apiClient = {
   async request<T>(url: string, options: RequestInit = {}): Promise<T> {
     const res = await fetch(baseUrl + url, {
       headers: {
@@ -9,35 +9,23 @@ export class ApiClient {
       },
       ...options,
     });
-
-    if (!res.ok) {
-      throw new Error("Request failed");
-    }
-
+    if (!res.ok) throw new Error("Request failed");
     return res.json();
-  }
+  },
 
   get<T>(url: string) {
     return this.request<T>(url);
-  }
+  },
 
   post<T, B>(url: string, body: B) {
-    return this.request<T>(url, {
-      method: "POST",
-      body: JSON.stringify(body),
-    });
-  }
+    return this.request<T>(url, { method: "POST", body: JSON.stringify(body) });
+  },
 
   put<T, B>(url: string, body: B) {
-    return this.request<T>(url, {
-      method: "PUT",
-      body: JSON.stringify(body),
-    });
-  }
+    return this.request<T>(url, { method: "PUT", body: JSON.stringify(body) });
+  },
 
   delete<T>(url: string) {
-    return this.request<T>(url, {
-      method: "DELETE",
-    });
-  }
-}
+    return this.request<T>(url, { method: "DELETE" });
+  },
+};
